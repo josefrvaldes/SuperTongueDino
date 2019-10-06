0e174dbab21c10eb81084fb06770883ccaebe567
Jose Valdés <josefrvaldes@gmail.com>
2019-10-06 19:09:41 +0200

    v0.9.1 - Faltaba un archivo

---
+++ img/patronCastillo.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="TilesCastillo" tilewidth="8" tileheight="8" tilecount="24" columns="6">
+ <image source="TilesCastillo.png" width="48" height="32"/>
+ <terraintypes>
+  <terrain name="Pared" tile="2"/>
+ </terraintypes>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="2" terrain="0,0,0,0"/>
+ <tile id="3" terrain="0,0,0,0"/>
+ <tile id="7" terrain="0,0,0,0"/>
+ <tile id="8" terrain="0,0,0,0"/>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="15" terrain="0,0,0,0"/>
+ <tile id="19" terrain="0,0,0,0"/>
+ <tile id="20" terrain="0,0,0,0"/>
+ <tile id="21" terrain="0,0,0,0"/>
+</tileset>

744baf16e139dc0b8a85001873bc1b921426527e
carlos <aslanadidas98@gmail.com>
2019-10-15 10:48:25 +0200

    v0.4 - Invisibilidad - movimiento sprite direccion/invisibilidad

--- src/man/sprite.s
+++ src/man/sprite.s
@@ -11,11 +11,13 @@
 .globl _hero_sp_3
 .globl _hero_sp_4
 .globl _hero_sp_5
+.globl _hero_sp_6
+.globl _hero_sp_7
 .globl _enemigo1_sp_0
 
-vx_guardada: .db #0
+vx_actual:   .db #0
 paso_sprite: .db #0         ;; 0 PASO1 // 1 PASO2
-tempo1:      .db #0x20
+tempo1:      .db #0x07
 ;;
 ;; METODO PARA CAMBIO DE SPRITE
 ;; INPUT:   A sprite  al que queremos cambiar
@@ -26,7 +28,8 @@ set_sprite_hero::
     ;call man_entity_getArray  ;; entidad que quiero en IX
     ld a, e_vx(ix)
     cp  #0
-    ret z
+    jr  z, procesar_invisibilidad_sprite          ;; si la velocidad en x no se modifica no entro al contador
+    ;; pero si debo permitir cambio de sprote de invisibilidad
 
     ld  a,  (paso_sprite)
     ld  b, a
@@ -35,9 +38,9 @@ set_sprite_hero::
     ld  a, (tempo1)
     dec  a
     ld  (tempo1), a
-    jr  nz, salto_tempo
+    jr  nz, procesar_cambios_sprite
         ;; entramos y reseteamos
-        ld  a, #0x20
+        ld  a, #0x07
         ld  (tempo1), a
 
         ;; ACTUAMOS
@@ -47,47 +50,39 @@ set_sprite_hero::
             ;; ES PASO 1
             ld  a, #1
             ld  (paso_sprite), a
-                call    sprite_hero_direction_paso1
-                call    sprite_visibility
-            jr  finish_sprite_hero
+            ;call    sprite_hero_direction_paso1
+            jr  procesar_cambios_sprite
 es_paso2:
             ;; ES PASO 2
             ld  a, #0
             ld  (paso_sprite), a
-                call    sprite_hero_direction_paso2
-                call    sprite_visibility
+            ;call    sprite_hero_direction_paso2
             ;ret
 
+procesar_cambios_sprite:
 
-   ; ret  z
-  ;  jr  nc, movIz
-        ;; NOS MOVEMOS HACIA LA DERECHA
 
-   ;         ld  a,  e_invisi(ix)
-    ;        dec a
-     ;       jr  z, invisb_der
-      ;      ld  hl, #_hero_sp_0
-       ;     jr finish_sprite_hero
-;invisb_der:
- ;           ld  hl, #_hero_sp_2
-  ;          jr finish_sprite_hero
+    ld  a, (paso_sprite)
+    dec a
+    jr  z, procesar_paso2
+        call    sprite_hero_paso1
+        jr  finish_sprite_hero
+procesar_paso2:
+        call    sprite_hero_paso2
+        jr  finish_sprite_hero
 
-;movIz:
-            ;; LENGUA PARA ABAJO
-                ;; invisible?
- ;           ld  a,  e_invisi(ix)
-  ;          dec a
-   ;         jr  z, invisb_iz
-    ;        ld  hl, #_hero_sp_1
-     ;       jr finish_sprite_hero
-;invisb_iz:
- ;           ld  hl, #_hero_sp_3
-  ;          jr finish_sprite_hero
+
+procesar_invisibilidad_sprite:
+    ;; MOVIMINETO HACIA LA DERECHA
+    ld  a,  e_invisi(ix)
+    dec a
+    ret nz
+        call sprite_hero_invisibilidad
 
 finish_sprite_hero:
         ld  e_pspr_h(ix), h
         ld  e_pspr_l(ix), l
-salto_tempo:
+
 
     ret
 
@@ -96,49 +91,94 @@ salto_tempo:
 ;; INPUT: B - PASO 1 O PASO 2 (EFECTO DE ANDAR)
 ;; RETURN: HL - DIRECCION DEL SPRITE
 ;;
-sprite_hero_direction_paso1:
+sprite_hero_paso1:
+
     add   e_x(ix)
     ld c, a
     ld a, e_vx(ix)
     sub    c
     jr  nc, movIz_paso1
+        ;; MOVIMINETO HACIA LA DERECHA
+        ld  a,  e_invisi(ix)
+        dec a
+        jr nz,  no_invisibilidad_paso1_der
+            ;; INVISIBLE
+            ld  hl, #_hero_sp_2
+        ret
+no_invisibilidad_paso1_der:
+            ;; VISIBLE
             ld  hl, #_hero_sp_0
         ret
-movIz_paso1:
-        ;; EN DERECHA  ES EL 1
-            ld  hl, #_hero_sp_1
-        ret
 
-;;
-;; METODO QUE MODIFICA EL SPRITE SEGUN LA DIRECCION A LA QUEMIRA
-;; INPUT: B - PASO 1 O PASO 2 (EFECTO DE ANDAR)
-;; RETURN: HL - DIRECCION DEL SPRITE
-;;
-sprite_hero_direction_paso2:
+movIz_paso1:
+    ;; MOVIMIENTO HACIA LA IZQUIERDA
+    ld  a,  e_invisi(ix)
+    dec a
+    jr nz,  no_invisibilidad_paso1_iz
+        ;; INVISIBLE
+        ld  hl, #_hero_sp_3
+    ret
+no_invisibilidad_paso1_iz:
+        ;; VISIBLE
+        ld  hl, #_hero_sp_1
+    ret
+
+
+
+sprite_hero_paso2:
+
     add   e_x(ix)
     ld c, a
     ld a, e_vx(ix)
     sub    c
     jr  nc, movIz_paso2
-        ;; EN DERECHA  ES EL  4
+        ;; MOVIMINETO HACIA LA DERECHA
+        ld  a,  e_invisi(ix)
+        dec a
+        jr nz,  no_invisibilidad_paso2_der
+            ;; INVISIBLE
+            ld  hl, #_hero_sp_6
+        ret
+no_invisibilidad_paso2_der:
+            ;; VISIBLE
             ld  hl, #_hero_sp_4
         ret
+
 movIz_paso2:
-        ;; EN DERECHA  ES EL  5
-            ld  hl, #_hero_sp_5
-        ret
-
-
-
-
-
-sprite_visibility:
-
+    ;; MOVIMIENTO HACIA LA IZQUIERDA
+    ld  a,  e_invisi(ix)
+    dec a
+    jr nz,  no_invisibilidad_paso2_iz
+        ;; INVISIBLE
+        ld  hl, #_hero_sp_7
+    ret
+no_invisibilidad_paso2_iz:
+        ;; VISIBLE
+        ld  hl, #_hero_sp_5
     ret
 
 
-guardar_VX::
-    ld  a, e_vx(ix)
-    ld  (vx_guardada), a
+
+
+
+sprite_hero_invisibilidad:
+    ;; INVISIBLE
+    add   e_x(ix)
+    ld c, a
+    ld a, (vx_actual)
+    sub    c
+    jr  nc, inv_iz
+        ;; MOVIMINETO HACIA LA DERECHA
+        ld  hl, #_hero_sp_2
+    ret
+inv_iz:
+    ;; MOVIMIENTO HACIA LA IZQUIERDA
+        ld  hl, #_hero_sp_3
+    ret
+;;
+;;
+;;
+guardar_VX:
+    ld  (vx_actual), a
 
     ret

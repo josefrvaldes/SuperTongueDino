5b2af9441ef07b223cad6f9b3c52c878b6063cc2
valdes <josefrvaldes@gmail.com>
2019-10-19 19:21:46 +0200

    v0.26 - Introduciendo tiles distintos a obstáculos con los que se puede colisionar o no

---
+++ img/tileset_juego.old.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="tileset_juego" tilewidth="8" tileheight="8" tilecount="48" columns="8">
+ <image source="tileset_juego.png" width="64" height="48"/>
+</tileset>

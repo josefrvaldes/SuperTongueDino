ad9defc77d11aa84330687b2459076bc4bce5743
Jose <josemartinez81998@gmail.com>
2019-10-30 11:00:30 +0100

    v0.67 - Licencia ficheros, tecla mutear musica, contador muertes endgame

--- src/cpct/cpct_setDrawCharM0.s
+++ src/cpct/cpct_setDrawCharM0.s
@@ -1,6 +1,6 @@
 ;;-----------------------------LICENSE NOTICE------------------------------------
 ;;  This file is part of CPCtelera: An Amstrad CPC Game Engine 
-;;  Copyright (C) 2018 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+;;  Copyright (C) 2018 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)  modified by Carlos de la Fuente / Jose Martinez / Jose Francisco Valdes / (@clover_gs)
 ;;
 ;;  This program is free software: you can redistribute it and/or modify
 ;;  it under the terms of the GNU Lesser General Public License as published by
--- src/ent/ent_obstacle.h.s
+++ src/ent/ent_obstacle.h.s
@@ -1,3 +1,22 @@
+;;----------------------------------LICENSE NOTICE-----------------------------------------------------
+;;    Super Tongue Dino is a challenging platform game
+;;    Copyright (C) 2019  Carlos de la Fuente / Jose Martinez / Jose Francisco Valdes / (@clover_gs)
+;;
+;;    This program is free software: you can redistribute it and/or modify
+;;    it under the terms of the GNU General Public License as published by
+;;    the Free Software Foundation, either version 3 of the License, or
+;;    (at your option) any later version.
+;;
+;;    This program is distributed in the hope that it will be useful,
+;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;    GNU General Public License for more details.
+;;
+;;    You should have received a copy of the GNU General Public License
+;;    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;------------------------------------------------------------------------------------------------------
+
+
 ;;
 ;; Entity OBSTACLE
 ;;
